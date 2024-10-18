e70c06d509fa75d30677f6bb6aa8de604a7f5b87
Benedicta Davour <benedictadavour777@gmail.com>
2024-10-18 14:55:29 -0700

    started working on products page

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,18 +1,21 @@
 import { toast } from "react-toastify";
 import { apiGetProducts } from "../../services/product";
+import { useEffect, useState } from "react";
 
 
 const Products = () => {
-    let products;
+    const[products, setProducts] = useState([]);
+  
     console.log("first : ",products)
+    // function to fetch from api
     const fetchData = async ()=> {
         
        try {
-        const products = await apiGetProducts();
+        const fetchedProducts = await apiGetProducts();
         //  products = [{name:"strapless bra", slug:"1-e"},
         //     {name:"sneakers", slug:"2-s"}];
-        console.log("Products: ", products);
-        return products;
+        console.log("Products: ", fetchedProducts);
+        setProducts(fetchedProducts);
 
        } catch (error) {
         toast.error(error.message);
@@ -20,10 +23,22 @@ const Products = () => {
         
        }
     };
-    fetchData();
-    console.log("Second : ", products);
+    useEffect(()=>{
+      fetchData();
+     
+    },[]);
+    
   return <div>
-
+    <h1>Here are the products</h1>
+    <div className="h-[80%] w-[80%] border flex flex-col">
+      {products.map((product)=> (
+        <div key={product.slug}>
+          {products.name}
+        </div>
+      )
+       
+      )}
+    </div>
   </div>;
   
 };
